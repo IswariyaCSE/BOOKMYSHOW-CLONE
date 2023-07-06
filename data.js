d2414fe1ed8b9ecd1c6a476d3f2665651a7e7a39
IswariyaCSE <107340178+IswariyaCSE@users.noreply.github.com>
2023-07-06 23:56:18 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,20 @@
+let movies = [
+    {
+        image: 'banner1.webp'
+    },
+    {
+        image: 'banner2.webp'
+    },
+    {
+        image: 'banner3.webp'
+    },
+    {
+        image: 'banner4.webp'
+    },
+    {
+        image: 'banner5.webp'
+    },
+    {
+        image: 'banner6.webp'
+    }
+]
